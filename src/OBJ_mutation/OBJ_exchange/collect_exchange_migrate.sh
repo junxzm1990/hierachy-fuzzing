06a1c612809e52f25b54989770f4be51641454cf
Yifan <yw03021103@gmail.com>
2021-09-13 20:23:13 -0400

    update object exchange script with removing mirgrating part

--- src/OBJ_mutation/OBJ_exchange/collect_exchange_migrate.sh
+++ src/OBJ_mutation/OBJ_exchange/collect_exchange_migrate.sh
@@ -23,76 +23,77 @@ while true; do
 	echo ">>>>>>>>>>>>>>>> NOW collecting OBJs from ALL <<<<<<<<<<<<<<<<<<<<<<<<"
 	
 	# collecting all objects from all PDF files generated from harness
-	for queue in `find $EVAL_BIN/result/test_run_$DATE -name "queue" | grep -v -e "afl_" -e "obj_"`; do 
-	
-		harness_queue_path=`dirname $queue`
-	
-		for seed in $harness_queue_path/queue/*; do 
-	
-			echo "[+] PROCESSING "$seed
-	
-			OBJ_bool=0
-	                while read line 
-	                do
-	                        if [[ "$line" == *"0 obj"* ]]; then
-	                                OBJ_bool=1
-	                               # (( cnt++ ))
-	                                echo $line >> $OUT_DIR/global
-	                        elif [[ "$line" == "endobj" ]]; then
-	                                OBJ_bool=0
-	                                echo $line >> $OUT_DIR/global
-	                        elif [[ $OBJ_bool == 1 ]]; then
-	                                echo $line >> $OUT_DIR/global
-	                        fi
-	                done < "$seed"
-		done
-	done
+#	for queue in `find $EVAL_BIN/result/test_run_$DATE -name "queue" | grep -v -e "afl_" -e "obj_"`; do 
+#	
+#		harness_queue_path=`dirname $queue`
+#	
+#		for seed in $harness_queue_path/queue/*; do 
+#	
+#			echo "[+] PROCESSING "$seed
+#	
+#			OBJ_bool=0
+#	                while read line 
+#	                do
+#	                        if [[ "$line" == *"0 obj"* ]]; then
+#	                                OBJ_bool=1
+#	                               # (( cnt++ ))
+#	                                echo $line >> $OUT_DIR/global
+#	                        elif [[ "$line" == "endobj" ]]; then
+#	                                OBJ_bool=0
+#	                                echo $line >> $OUT_DIR/global
+#	                        elif [[ $OBJ_bool == 1 ]]; then
+#	                                echo $line >> $OUT_DIR/global
+#	                        fi
+#	                done < "$seed"
+#		done
+#	done
 	
 	pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/ | wc -l`
 	
 	for queue in `find $EVAL_BIN/result/test_run_$DATE -name "queue" | grep -v -e "afl_" -e "obj_"`; do
 	
-		harness_queue_path=`dirname $queue`
+                        mkdir $OUT_DIR/tmp_obj_exchange/
 	
-		for seed in $harness_queue_path/queue/*; do 
-		
-			seed_name=$(basename "$seed")
-	
-	                mkdir $OUT_DIR/$seed_name
-	
-			python2.7 $SRC/src/OBJ_mutation/OBJ_exchange/generator.py $OUT_DIR/global $seed $OUT_DIR/$seed_name/
+			python2.7 $SRC/src/OBJ_mutation/OBJ_exchange/new_generator.py $SRC/src/OBJ_mutation/OBJ_exchange/class_obj.json $queue $OUT_DIR/tmp_obj_exchange/ 
 	
 	                # migrate new generated seeds to queue
-	                for new in $OUT_DIR/$seed_name/*; do
-		
-				len=${#pre_cnt}
-	                        bond=`expr 5 - $len`
-	                        zero=0
-	
-	                        for z in $(seq $bond)
-	                        do
-	                                zero=$zero"0"
-	                        done
-	
-	                        base=`echo $(basename $seed_name) | cut -d , -f 2`
-	 
-	                        tp=$(basename $new)
-	
-				echo "hahahah : "$base","$tp
-	
-	                        name="id:"$zero$pre_cnt","$base","$tp
-	                	
-				mv $new $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
-	  	
-				echo "[-] OUTPUT "$new
-	
-				let "pre_cnt=pre_cnt+1"
-	 		done
-	
-			rm -rf $OUT_DIR/$seed_name
-		done
+#                        cur_cnt=`ls $OUT_DIR/tmp_obj_exchange/ | wc -l`
+                       # while [ $cur_cnt \> 0 ]; do
+#	                        for new in $OUT_DIR/tmp_obj_exchange/*; do
+#                                        mkdir $OUT_DIR/tmp_obj_exchange/trim/
+#                                        mv $new $OUT_DIR/tmp_obj_exchange/trim/
+#                                        # TRIM PDFs ------------------------------------
+#                                        python3.5 $SRC/scripts/trim_tools/new_trim_pdf.py -i $OUT_DIR/tmp_obj_exchange/trim/ -b $COMMAND -s $AFL/afl-showmap -m none -t 100000 -o $OUT_DIR/tmp_obj_exchange/
+#                                        rm -rf $OUT_DIR/tmp_obj_exchange/trim/
+#
+#                                        # RENAMing before migrating
+#		        		len=${#pre_cnt}
+#	                                bond=`expr 5 - $len`
+#	                                zero=0
+#	
+#	                                for z in $(seq $bond)
+#	                                do
+#	                                        zero=$zero"0"
+#	                                done
+#	
+#	                                base=`echo $(basename $i) | cut -d , -f 2`
+#	 
+#	                                #tp=$(basename $new)
+#	
+#	                                name="id:"$zero$pre_cnt","$base
+#	                        	
+#		        		mv $new $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
+#	  	        
+#		        		echo "[-] OUTPUT "$new
+#	
+#		        		let "pre_cnt=pre_cnt+1"
+#	 	        	done
+#                       # done
+#	
+#		#	rm -rf $OUT_DIR/$seed_name
+#		#done
+
 	
 	done
 	                     
-	rm -rf $OUT_DIR/global
 done	
