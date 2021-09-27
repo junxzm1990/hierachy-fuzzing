9364c7ef5deb0a7a6810b32f45a9bb3aaeb5e5da
Yifan <yw03021103@gmail.com>
2021-09-27 16:00:53 -0400

    update main.sh

--- src/main.sh
+++ src/main.sh
@@ -82,7 +82,7 @@ v_NUM=`expr $NUM + 3` # running vanilla with 3 more, since harness
 $GO run $MAIN_GO -afl $AFL/afl-fuzz -i $EVAL_BIN/bin/vanilla_seed/ -no-master -name afl -m none -t 100000 -o $EVAL_BIN/result/vanilla_test_run_$DATE -n $v_NUM -- $COMMAND
 
 # handling harness generating, filtering, compiling, fuzzing and output migration
-bash $SRC/src/harness/harness_compile_fuzzing_migrate.sh -c $CONFIG
+#bash $SRC/src/harness/mupdf_new_harness_compile_fuzzing_migrate.sh -c $CONFIG
 
 # handling objects exhanging based on object types
 #bash $SRC/src/OBJ_mutation/OBJ_exchang/collect_exchange_migrate.sh -c $CONFIG
