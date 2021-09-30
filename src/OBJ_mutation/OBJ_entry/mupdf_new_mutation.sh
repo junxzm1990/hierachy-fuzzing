80e65ca7a5ae62f8f0abed3f12cd5150c54d99cb
Yifan <yw03021103@gmail.com>
2021-09-30 14:38:03 -0400

    updated with obj entry mutation seeds migration

--- src/OBJ_mutation/OBJ_entry/mupdf_new_mutation.sh
+++ src/OBJ_mutation/OBJ_entry/mupdf_new_mutation.sh
@@ -45,83 +45,87 @@ if [[ $EVAL_BIN == "" ]]; then
 fi
 
 
-sleep 5m
-mkdir $OUT_DIR/entry_gen/
-while true; do
-        pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue |wc -l`
-#       for queue in `find $EVAL_BIN/result/test_run_$DATE/ -name "queue" | grep -v -e "afl_" -e "obj_"`; do 
-#       
-#               harness_queue_path=`dirname $queue`
+#sleep 5m
+if  [ -z "$(ls -A $OUT_DIR/pdf_gen/)" ]; then
 
-                for seed in $OUT_DIR/pdf_gen/*; do
+	echo "NO NEW SEED YET-LET's WAIT"
 
-                        echo ")))))))))))))))))))))))))))))) processing $seed "
+else
+ 
+	mkdir $OUT_DIR/entry_gen/
 
-                        diff -y -a $EVAL_BIN/bin/init/pdf_org.pdf $seed > $OUT_DIR/org_diff
+	while true; do
 
-                        python2.7 $SRC/src/OBJ_mutation/OBJ_entry/grammar_reserved_mutation.py $OUT_DIR/org_diff $seed $OUT_DIR/entry_gen/
-			for i in $OUT_DIR/entry_gen/*; do
-                                # TRIM PDFs : trim PDFs before renaming
-                                mkdir $OUT_DIR/entry_gen_trim_in/
-                                mkdir $OUT_DIR/entry_gen_trim_out/
-
-                                cp $i $OUT_DIR/entry_gen_trim_in/
-
-                                python3 $SRC/scripts/trim_tool/new_trim_pdf.py -i $OUT_DIR/entry_gen_trim_in/ -b $COMMAND -s $AFLpp_loc/afl-showmap -m none -t 100000 -o $OUT_DIR/entry_gen_trim_out/
-
-                                if [ -z "$(ls -A $OUT_DIR/entry_gen_trim_out/)" ]; then
-                                         # RENAMing before migrating
-                                         len=${#pre_cnt}
-                                         bond=`expr 5 - $len`
-                                         zero=0
-
-                                         for z in $(seq $bond)
-                                         do
-                                                 zero=$zero"0"
-                                         done
-                                         echo $i
-                                         base=`echo $(basename $i) | cut -d , -f 2`
-
-                                         name="id:"$zero$pre_cnt","$base
-
-                                         mv $i $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue/$name
-                                 else
-                                         # Compare trimed and untrimed, which one is smaller
-                                         I=`wc -c $OUT_DIR/entry_gen_trim_in/* | cut -d ' ' -f 1`
-                                         O=`wc -c $OUT_DIR/entry_gen_trim_out/* | cut -d ' ' -f 1`
-                                         if [ "$I" -gt "$O" ]; then
-                                                 ## 3.2.2 RENAMING : rename reduced size PDFs
-                                                 len=${#pre_cnt}
-                                                 bond=`expr 5 - $len`
-                                                 zero=0
-
-                                                 for z in $(seq $bond)
-                                                 do
-                                                         zero=$zero"0"
-                                                 done
-
-                                                 base=`echo $(basename $i) | cut -d , -f 2`
-
-						 name="id:"$zero$pre_cnt","$base
-
-                                                 mv $OUT_DIR/entry_gen_trim_in/* $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue/$name
-
-                                         fi
-
-
-
-                                 fi
-
-                                 let "pre_cnt=pre_cnt+1"
-                                 rm -rf $OUT_DIR/entry_gen_trim_in/
-                                 rm -rf $OUT_DIR/entry_gen_trim_out/
-                        done
-
-                        rm -rf $OUT_DIR/org_diff
-
-                        rm -rf $OUT_DIR/entry_gen/*
-
-                done
-#       done
-done
+	        pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue |wc -l`
 	
+	        for seed in $OUT_DIR/pdf_gen/*; do
+	
+	                echo ")))))))))))))))))))))))))))))) processing $seed "
+	
+	                diff -y -a $EVAL_BIN/bin/init/pdf_org.pdf $seed > $OUT_DIR/org_diff
+	
+	                python2.7 $SRC/src/OBJ_mutation/OBJ_entry/grammar_reserved_mutation.py $OUT_DIR/org_diff $seed $OUT_DIR/entry_gen/
+			for i in $OUT_DIR/entry_gen/*; do
+	                        # TRIM PDFs : trim PDFs before renaming
+	                        mkdir $OUT_DIR/entry_gen_trim_in/
+	                        mkdir $OUT_DIR/entry_gen_trim_out/
+	
+	                        cp $i $OUT_DIR/entry_gen_trim_in/
+	
+	                        python3 $SRC/scripts/trim_tool/new_trim_pdf.py -i $OUT_DIR/entry_gen_trim_in/ -b $COMMAND -s $AFLpp_loc/afl-showmap -m none -t 100000 -o $OUT_DIR/entry_gen_trim_out/
+	
+	                        if [ -z "$(ls -A $OUT_DIR/entry_gen_trim_out/)" ]; then
+	                                 # RENAMing before migrating
+	                                 len=${#pre_cnt}
+	                                 bond=`expr 5 - $len`
+	                                 zero=0
+	
+	                                 for z in $(seq $bond)
+	                                 do
+	                                         zero=$zero"0"
+	                                 done
+	                                 echo $i
+	                                 base=`echo $(basename $i) | cut -d , -f 2`
+	
+	                                 name="id:"$zero$pre_cnt","$base
+	
+	                                 mv $i $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue/$name
+	                         else
+	                                 # Compare trimed and untrimed, which one is smaller
+	                                 I=`wc -c $OUT_DIR/entry_gen_trim_in/* | cut -d ' ' -f 1`
+	                                 O=`wc -c $OUT_DIR/entry_gen_trim_out/* | cut -d ' ' -f 1`
+	                                 if [ "$I" -gt "$O" ]; then
+	                                         ## 3.2.2 RENAMING : rename reduced size PDFs
+	                                         len=${#pre_cnt}
+	                                         bond=`expr 5 - $len`
+	                                         zero=0
+	
+	                                         for z in $(seq $bond)
+	                                         do
+	                                                 zero=$zero"0"
+	                                         done
+	
+	                                         base=`echo $(basename $i) | cut -d , -f 2`
+	
+						 name="id:"$zero$pre_cnt","$base
+	
+	                                         mv $OUT_DIR/entry_gen_trim_in/* $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue/$name
+	
+	                                 fi
+	
+	
+	
+	                         fi
+	
+	                         let "pre_cnt=pre_cnt+1"
+	                         rm -rf $OUT_DIR/entry_gen_trim_in/
+	                         rm -rf $OUT_DIR/entry_gen_trim_out/
+	                done
+	
+	                rm -rf $OUT_DIR/org_diff
+	
+	                rm -rf $OUT_DIR/entry_gen/*
+	
+	        done
+	done
+fi	
