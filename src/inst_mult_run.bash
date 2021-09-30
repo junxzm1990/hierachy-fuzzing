f12964266cece9c73dc8322f5c9254115a5034af
Yifan <yw03021103@gmail.com>
2021-09-30 00:46:32 -0400

    seperate inst_mult_run from main

--- src/inst_mult_run.bash
+++ src/inst_mult_run.bash
@@ -9,9 +9,21 @@ done
 . $CONFIG
 
 # run  harness AFL++ fuzzing
-$GO run $MAIN_GO -afl $AFL/afl-fuzz -i $EVAL_BIN/bin/seed/ -no-master -name afl -m none -t 100000 -o $EVAL_BIN/result/test_run_$DATE -n $NUM -- $COMMAND
+$GO run $MAIN_GO -afl $AFLpp_loc/afl-fuzz -i $EVAL_BIN/bin/seed/ -no-master -name afl -m none -t 100000 -o $EVAL_BIN/result/test_run_$DATE -n $NUM -- $COMMAND
+
+# these two dir has to be created there no move
+mkdir $EVAL_BIN/result/test_run_$DATE/harness_gen/
+
+mkdir $EVAL_BIN/result/test_run_$DATE/harness_gen/queue/
+
+mkdir $EVAL_BIN/result/test_run_$DATE/obj_exchange/
+
+mkdir $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/
+
+mkdir $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/
+
+mkdir $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue
 
 # run villnia AFL++ fuzzing
 v_NUM=`expr $NUM + 3` # running vanilla with 3 more, since harness
-$GO run $MAIN_GO -afl $AFL/afl-fuzz -i $EVAL_BIN/bin/vanilla_seed/ -no-master -name afl -m none -t 100000 -o $EVAL_BIN/result/vanilla_test_run_$DATE -n $v_NUM -- $COMMAND
-
+$GO run $MAIN_GO -afl $AFLpp_loc/afl-fuzz -i $EVAL_BIN/bin/vanilla_seed/ -no-master -name afl -m none -t 100000 -o $EVAL_BIN/result/vanilla_test_run_$DATE -n $v_NUM -- $COMMAND
