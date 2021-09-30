01d30d209f0aa1a94e3e8e5003d7cd59118a515b
Yifan <yw03021103@gmail.com>
2021-09-30 15:23:04 -0400

    update obj exchange seeds scheduling

--- src/OBJ_mutation/OBJ_exchange/mupdf_collect_exchange_migrate.sh
+++ src/OBJ_mutation/OBJ_exchange/mupdf_collect_exchange_migrate.sh
@@ -16,19 +16,20 @@ fi
 . $CONFIG
 
 
-if  [ -z "$(ls -A $OUT_DIR/pdf_gen/)" ]; then
+while true; do
 
-	echo "NO NEW SEEDS YET-LET's WAIT"
+	if  [ -z "$(ls -A $OUT_DIR/pdf_gen/)" ]; then
+	
+		echo "NO NEW SEEDS YET-LET's WAIT"
+	
+	else 
 
-else 
+        	echo ">>>>>>>>>>>>>>>> NOW collecting OBJs from ALL <<<<<<<<<<<<<<<<<<<<<<<<"
+        	pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/ | wc -l`
+        	mkdir $OUT_DIR/tmp_obj_exchange/
 
-	while true; do
-	        echo ">>>>>>>>>>>>>>>> NOW collecting OBJs from ALL <<<<<<<<<<<<<<<<<<<<<<<<"
-	        pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/ | wc -l`
-	        mkdir $OUT_DIR/tmp_obj_exchange/
-	
-	        python2.7 $SRC/src/OBJ_mutation/OBJ_exchange/new_generator.py $SRC/src/OBJ_mutation/OBJ_exchange/class_obj.json $OUT_DIR/pdf_gen/ $OUT_DIR/tmp_obj_exchange/
-	
-	
-	done
-fi 
+        	python2.7 $SRC/src/OBJ_mutation/OBJ_exchange/new_generator.py $SRC/src/OBJ_mutation/OBJ_exchange/class_obj.json $OUT_DIR/pdf_gen/ $OUT_DIR/tmp_obj_exchange/
+		
+		
+	fi 
+done
--- src/OBJ_mutation/OBJ_exchange/mupdf_new_migrate.sh
+++ src/OBJ_mutation/OBJ_exchange/mupdf_new_migrate.sh
@@ -18,94 +18,96 @@ fi
 # create object exchange mutation queue, parallal with afl_S_X/queue and harness/queue
 
 #sleep 10m
-if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange/)" ]; then
+while true; do
 
-	echo "NO NEW SEEDS YET-BE PATIENT"
-
-else 
-        while true; do
-                echo ">>>>>>>>>>>>>>>> SCANNING TMP dir <<<<<<<<<<<<<<<<<<<<<<<<"
-        
-                pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/ | wc -l`
-                # migrate new generated seeds to queue
-                if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange/)" ]; then
-                        echo "Current TMP dir is empty, waiting new seeds ... "
-                else
-                        for new in $OUT_DIR/tmp_obj_exchange/*; do
-                                mkdir $OUT_DIR/tmp_obj_exchange_trim_in/
-                                mkdir $OUT_DIR/tmp_obj_exchange_trim_out/
-                                cp $new $OUT_DIR/tmp_obj_exchange_trim_in/
-                                # TRIM PDFs ------------------------------------
-                                python3 $SRC/scripts/trim_tool/new_trim_pdf.py -i $OUT_DIR/tmp_obj_exchange_trim_in/ -b $COMMAND -s $AFLpp_loc/afl-showmap -m none -t 100000 -o $OUT_DIR/tmp_obj_exchange_trim_out/
-                                if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange_trim_out/)" ]; then
-                                        # RENAMing before migrating
-                                        len=${#pre_cnt}
-                                        bond=`expr 5 - $len`
-                                        zero=0
-        
-                                        for z in $(seq $bond)
-                                        do
-                                                zero=$zero"0"
-                                        done
-                                        echo $new
-                                        base=`echo $(basename $new) | cut -d , -f 2`
-        
-                                        name="id:"$zero$pre_cnt","$base
-        
-                                        mv $new $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
-                                else
-                                        # Compare trimed and untrimed, which one is smaller
-                                        I=`wc -c $OUT_DIR/tmp_obj_exchange_trim_in/* | cut -d ' ' -f 1`
-                                        O=`wc -c $OUT_DIR/tmp_obj_exchange_trim_out/* | cut -d ' ' -f 1`
-        	                        if [ "$I" -gt "$O" ]; then
-                                                ## 3.2.2 RENAMING : rename reduced size PDFs
-                                                len=${#pre_cnt}
-                                                bond=`expr 5 - $len`
-                                                zero=0
-        
-                                                for z in $(seq $bond)
-                                                do
-                                                        zero=$zero"0"
-                                                done
-        
-                                                base=`echo $(basename $new) | cut -d , -f 2` 
-        
-                                                name="id:"$zero$pre_cnt","$base
-        
-                                                mv $OUT_DIR/tmp_obj_exchange_trim_out/* $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
-                                        else
-                                                ## 3.2.2 RENAMING : rename reduced size PDFs
-                                                len=${#pre_cnt}
-                                                bond=`expr 5 - $len`
-                                                zero=0
-        
-                                                for z in $(seq $bond)
-                                                do
-                                                        zero=$zero"0"
-                                                done
-        
-                                                base=`echo $(basename $new) | cut -d . -f 1` 
-        
-                                                name="id:"$zero$pre_cnt","$base
-        
-                                                mv $OUT_DIR/tmp_obj_exchange_trim_in/* $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
-        
-                                        fi
-        
-        
-        
-                                fi
-                                rm -rf $OUT_DIR/tmp_obj_exchange_trim_in/
-                                rm -rf $OUT_DIR/tmp_obj_exchange_trim_out/
-        
-                                echo "[-] OUTPUT "$new
-        
-                                let "pre_cnt=pre_cnt+1"
-                        done
-        
-                        rm -rf $OUT_DIR/tmp_obj_exchange/*
-        
-                fi
-        
-        done
-fi 
+	if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange/)" ]; then
+	
+		echo "NO NEW SEEDS YET-BE PATIENT"
+	
+	else 
+	        echo ">>>>>>>>>>>>>>>> SCANNING TMP dir <<<<<<<<<<<<<<<<<<<<<<<<"
+	
+	        pre_cnt=`ls $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/ | wc -l`
+	        # migrate new generated seeds to queue
+	        if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange/)" ]; then
+	                echo "Current TMP dir is empty, waiting new seeds ... "
+	        else
+	                for new in $OUT_DIR/tmp_obj_exchange/*; do
+	                        mkdir $OUT_DIR/tmp_obj_exchange_trim_in/
+	                        mkdir $OUT_DIR/tmp_obj_exchange_trim_out/
+	                        cp $new $OUT_DIR/tmp_obj_exchange_trim_in/
+	                        # TRIM PDFs ------------------------------------
+	                        python3 $SRC/scripts/trim_tool/new_trim_pdf.py -i $OUT_DIR/tmp_obj_exchange_trim_in/ -b $COMMAND -s $AFLpp_loc/afl-showmap -m none -t 100000 -o $OUT_DIR/tmp_obj_exchange_trim_out/
+	                        if [ -z "$(ls -A $OUT_DIR/tmp_obj_exchange_trim_out/)" ]; then
+	                                # RENAMing before migrating
+	                                len=${#pre_cnt}
+	                                bond=`expr 5 - $len`
+	                                zero=0
+	
+	                                for z in $(seq $bond)
+	                                do
+	                                        zero=$zero"0"
+	                                done
+	                                echo $new
+	                                base=`echo $(basename $new) | cut -d , -f 2`
+	
+	                                name="id:"$zero$pre_cnt","$base
+	
+	                                mv $new $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
+	                        else
+	                                # Compare trimed and untrimed, which one is smaller
+	                                I=`wc -c $OUT_DIR/tmp_obj_exchange_trim_in/* | cut -d ' ' -f 1`
+	                                O=`wc -c $OUT_DIR/tmp_obj_exchange_trim_out/* | cut -d ' ' -f 1`
+		                        if [ "$I" -gt "$O" ]; then
+	                                        ## 3.2.2 RENAMING : rename reduced size PDFs
+	                                        len=${#pre_cnt}
+	                                        bond=`expr 5 - $len`
+	                                        zero=0
+	
+	                                        for z in $(seq $bond)
+	                                        do
+	                                                zero=$zero"0"
+	                                        done
+	
+	                                        base=`echo $(basename $new) | cut -d , -f 2` 
+	
+	                                        name="id:"$zero$pre_cnt","$base
+	
+	                                        mv $OUT_DIR/tmp_obj_exchange_trim_out/* $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
+	                                else
+	                                        ## 3.2.2 RENAMING : rename reduced size PDFs
+	                                        len=${#pre_cnt}
+	                                        bond=`expr 5 - $len`
+	                                        zero=0
+	
+	                                        for z in $(seq $bond)
+	                                        do
+	                                                zero=$zero"0"
+	                                        done
+	
+	                                        base=`echo $(basename $new) | cut -d . -f 1` 
+	
+	                                        name="id:"$zero$pre_cnt","$base
+	
+	                                        mv $OUT_DIR/tmp_obj_exchange_trim_in/* $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/$name
+	
+	                                fi
+	
+	
+	
+	                        fi
+	                        rm -rf $OUT_DIR/tmp_obj_exchange_trim_in/
+	                        rm -rf $OUT_DIR/tmp_obj_exchange_trim_out/
+	
+	                        echo "[-] OUTPUT "$new
+	
+	                        let "pre_cnt=pre_cnt+1"
+	                done
+	
+	                rm -rf $OUT_DIR/tmp_obj_exchange/*
+	
+	        fi
+	
+	fi
+ 
+done
