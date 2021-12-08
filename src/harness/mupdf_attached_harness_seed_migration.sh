1f29fdb373cd9ad91f62e6efdcb9e6da4c4d9320
Yifan <yw03021103@gmail.com>
2021-12-07 22:26:26 -0500

    updated

--- src/harness/mupdf_attached_harness_seed_migration.sh
+++ src/harness/mupdf_attached_harness_seed_migration.sh
@@ -27,45 +27,45 @@ while true; do
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/harness_gen/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for harness_gen_seed in $MAIN_GROUP/result/test_run_$DATE/harness_gen/*;
+		for harness_gen_seed in $MAIN_GROUP/result/test_run_$DATE/harness_gen/queue/*;
 		do
 
-			if grep -q "$harness_gen_seed" $EVAL_BIN/result/test_run_$DATE/done_harness_list; 
+			if grep -q "$harness_gen_seed" $EVAL_BIN/result/test_run_$DATE/harness_gen/done_harness_list; 
 			then
 				echo "processed this seed"
 			else 
-				cp $harness_gen_seed $EVAL_BIN/result/test_run_$DATE/harness_gen/
-				echo $harness_gen_seed >> $EVAL_BIN/result/test_run_$DATE/done_harness_list
+				cp $harness_gen_seed $EVAL_BIN/result/test_run_$DATE/harness_gen/queue/
+				echo $harness_gen_seed >> $EVAL_BIN/result/test_run_$DATE/harness_gen/done_harness_list
 			fi
 
 		done
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/obj_exchange/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for obj_exchange_seed in $MAIN_GROUP/result/test_run_$DATE/obj_exchange/*;
+		for obj_exchange_seed in $MAIN_GROUP/result/test_run_$DATE/obj_exchange/queue/*;
 		do
 
-			if grep -q "$obj_exchange_seed" $EVAL_BIN/result/test_run_$DATE/done_obj_exchange_list; 
+			if grep -q "$obj_exchange_seed" $EVAL_BIN/result/test_run_$DATE/obj_exchange/done_obj_exchange_list; 
 			then
 				echo "processed this seed"
 			else 
-				cp $obj_exchange_seed $EVAL_BIN/result/test_run_$DATE/obj_exchange/
-				echo $obj_exchange_seed >> $EVAL_BIN/result/test_run_$DATE/done_obj_exchange_list
+				cp $obj_exchange_seed $EVAL_BIN/result/test_run_$DATE/obj_exchange/queue/
+				echo $obj_exchange_seed >> $EVAL_BIN/result/test_run_$DATE/obj_exchange/done_obj_exchange_list
 			fi
 
 		done
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/obj_entry_mutation/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for obj_entry_seed in $MAIN_GROUP/result/test_run_$DATE/obj_entry_mutation/*;
+		for obj_entry_seed in $MAIN_GROUP/result/test_run_$DATE/obj_entry_mutation/queue/*;
 		do
 
-			if grep -q "$obj_entry_seed" $EVAL_BIN/result/test_run_$DATE/done_obj_entry_list; 
+			if grep -q "$obj_entry_seed" $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/done_obj_entry_list; 
 			then
 				echo "processed this seed"
 			else 
-				cp $obj_entry_seed $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/
-				echo $obj_entry_seed >> $EVAL_BIN/result/test_run_$DATE/done_obj_entry_list
+				cp $obj_entry_seed $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/queue/
+				echo $obj_entry_seed >> $EVAL_BIN/result/test_run_$DATE/obj_entry_mutation/done_obj_entry_list
 			fi
 
 		done
