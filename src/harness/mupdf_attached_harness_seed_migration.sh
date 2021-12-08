c4897aea4f771614ffba5a80022d8af779cfdb44
Yifan <yw03021103@gmail.com>
2021-12-07 20:51:11 -0500

    update

--- src/harness/mupdf_attached_harness_seed_migration.sh
+++ src/harness/mupdf_attached_harness_seed_migration.sh
@@ -27,7 +27,7 @@ while true; do
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/harness_gen/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for harness_gen_seed in $MAIN_GROUP/harness_gen/*;
+		for harness_gen_seed in $MAIN_GROUP/result/test_run_$DATE/harness_gen/*;
 		do
 
 			if grep -q "$harness_gen_seed" $EVAL_BIN/result/test_run_$DATE/done_harness_list; 
@@ -42,7 +42,7 @@ while true; do
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/obj_exchange/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for obj_exchange_seed in $MAIN_GROUP/obj_exchange/*;
+		for obj_exchange_seed in $MAIN_GROUP/result/test_run_$DATE/obj_exchange/*;
 		do
 
 			if grep -q "$obj_exchange_seed" $EVAL_BIN/result/test_run_$DATE/done_obj_exchange_list; 
@@ -57,7 +57,7 @@ while true; do
 
 		echo ">>>>>>>>>>>>>>>> NOW cp seeds from main_target/obj_entry_mutation/ <<<<<<<<<<<<<<<<<<<<<<<<"
 		
-		for obj_entry_seed in $MAIN_GROUP/obj_entry_mutation/*;
+		for obj_entry_seed in $MAIN_GROUP/result/test_run_$DATE/obj_entry_mutation/*;
 		do
 
 			if grep -q "$obj_entry_seed" $EVAL_BIN/result/test_run_$DATE/done_obj_entry_list; 
